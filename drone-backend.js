618206e111569a6e93d6f82186556c021136c757
cca <contact@camrbidgecodind.com>
2015-08-14 12:28:17 +0100

    bugfixes

--- drone-backend.js
+++ drone-backend.js
@@ -11,55 +11,46 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
-    device("nav", {
+    .device("nav", {
     driver: "ardrone-nav",
     connection: "ardrone"
 })
     .on("ready", fly);
-    
-// Fly the bot
-function fly(robot) {
-bot=robot;
-}
 
-
-robot.drone.takeoff();
-.device("drone",{
-    driver:"ardrone",
-    connection:"ardrone"
-})
 var bot;
 function fly(robot) {
     bot = robot;
     bot.drone.config('general:navdata_demo', 'TRUE');
 
     bot.nav.on("navdata", function (data) {
+    });
         bot.drone.disableEmergency();
         bot.drone.ftrim();
         bot.drone.takeoff();
-        after(5 * 1000, function () {
+        after(10 * 1000, function () {
             bot.drone.right(0.2);
         });
-        after(6 * 1000, function () {
+    
+        after(11 * 1000, function () {
             bot.drone.right(0);
         });
-        after(7 * 1000, function () {
+        after(12 * 1000, function () {
             bot.drone.left(0.1);
         });
-        after(9 * 1000, function () {
+        after(13 * 1000, function () {
             bot.drone.left(0);
         });
-        after(14 * 1000, function () {
+        after(18 * 1000, function () {
             bot.drone.land();
         });
-        after(18 * 1000, function () {
+        after(23 * 1000, function () {
             bot.drone.stop();
         });
-    });
+
 }
 
-bot.nav.on("navdata", function(data) {
-    console.log(data);
-});
+//bot.nav.on("navdata", function(data) {
+//    console.log(data);
+//});
 
 Cylon.start();
